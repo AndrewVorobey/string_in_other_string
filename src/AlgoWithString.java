fc4b4930308b806a3b5a20f689b1ac325d6f7579
AndrewVorobey <andrew_vorobey@mail.ru>
2014-09-19 22:06:33 +0400

    Написан интерфейс

---
+++ src/AlgoWithString.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Андрей on 19.09.2014.
+ */
+public class AlgoWithString {
+    public static int getStringPosFromSequence(String in){
+        return 0;
+    }
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,10 +1,14 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Андрей on 19.09.2014.
  */
 public class MainClass {
     public static void main(String[] args) throws IOException {
-
+        BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in));
+        String inString = inputStream.readLine();
+        System.out.println(AlgoWithString.getStringPosFromSequence(inString));
     }
 }
